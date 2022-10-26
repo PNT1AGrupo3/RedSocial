8a35725e2c29df87c17670274a68173c4e319463
PNT1AGrupo3 <grupo3pnt@gmail.com>
2022-10-25 23:05:33 -0300

    Initial commit

---
+++ RedSocial/Models/Imagen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RedSocial.Models
+{
+    public class Imagen
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int imagenId { get; set; }
+        public string fullPath { get; set; } 
+        public string getFileName()
+        {
+            return "ver como se conforma el full path en el web server";
+        }
+    }
+    
+}
---
+++ RedSocial/Models/Publicacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedSocial.Models
+{
+    public class Publicacion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int putlicationId { get; set; }
+        public DateTime fecha { get; set; }
+        public string texto { get; set; }
+        public List<Usuario> likes;
+        public List<Comentario> comentarios;
+        public List<Imagen> imagenes;
+        public Publicacion()
+        {
+            likes = new List<Usuario>();
+            comentarios = new List<Comentario>();
+            imagenes = new List<Imagen>();
+        }
+    }
+}

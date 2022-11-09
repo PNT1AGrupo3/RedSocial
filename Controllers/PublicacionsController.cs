06f954de0704e020f876488ba29684a3c90482f5
PNT1AGrupo3 <grupo3pnt@gmail.com>
2022-11-09 20:54:23 -0300

    cruds

---
+++ Controllers/PublicacionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RedSocial.Context;
+using RedSocial.Models;
+
+namespace RedSocial.Controllers
+{
+    public class PublicacionsController : Controller
+    {
+        private readonly RedSocialDatabaseContext _context;
+
+        public PublicacionsController(RedSocialDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Publicacions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Publicaciones.ToListAsync());
+        }
+
+        // GET: Publicacions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publicacion = await _context.Publicaciones
+                .FirstOrDefaultAsync(m => m.putlicationId == id);
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(publicacion);
+        }
+
+        // GET: Publicacions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publicacions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("putlicationId,fecha,texto")] Publicacion publicacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publicacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publicacion);
+        }
+
+        // GET: Publicacions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publicacion = await _context.Publicaciones.FindAsync(id);
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+            return View(publicacion);
+        }
+
+        // POST: Publicacions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("putlicationId,fecha,texto")] Publicacion publicacion)
+        {
+            if (id != publicacion.putlicationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publicacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublicacionExists(publicacion.putlicationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publicacion);
+        }
+
+        // GET: Publicacions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var publicacion = await _context.Publicaciones
+                .FirstOrDefaultAsync(m => m.putlicationId == id);
+            if (publicacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(publicacion);
+        }
+
+        // POST: Publicacions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var publicacion = await _context.Publicaciones.FindAsync(id);
+            _context.Publicaciones.Remove(publicacion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublicacionExists(int id)
+        {
+            return _context.Publicaciones.Any(e => e.putlicationId == id);
+        }
+    }
+}
---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RedSocial.Context;
+using RedSocial.Models;
+
+namespace RedSocial.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly RedSocialDatabaseContext _context;
+
+        public UsuariosController(RedSocialDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuarios.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.userId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("userId,password,email,preguntaSecreta,respuestaSecreta,fechaCreacion")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("userId,password,email,preguntaSecreta,respuestaSecreta,fechaCreacion")] Usuario usuario)
+        {
+            if (id != usuario.userId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.userId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.userId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.userId == id);
+        }
+    }
+}
